ba578f51bf3bfac52027dfd06c67ff687ed6da35
skulath-1 <103314965+skulath-1@users.noreply.github.com>
2022-07-03 11:57:57 +0100

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,48 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("./assets/player.png");
+
+    this.life1 = "green";
+    this.life2 = "green";
+    this.life3 = "green";
+
+    World.add(world, this.body);
+  }
+
+  life() {
+    push();
+   //write a correct code to show lives as three green rectangles in a strainght line.
+   fill("white");
+   textSize(20)
+   text("Player", width - 1400, 80);
+
+   fill("green")
+   rect (100,100,70,30)
+   rect(170,100,70,30)
+   rect(240,100,70,30)
+   
+    pop();
+  }
+
+ 
+
+
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
